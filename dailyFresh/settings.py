9e278c05f343248546b4b09876eea5705c247f04
Yvonne <Ywuyvonne@gmail.com>
2023-02-02 12:30:30 -0500

    Constructed the template HTML file and finished remodeling other pages files. import template HTML to simplify the code.

--- dailyFresh/settings.py
+++ dailyFresh/settings.py
@@ -172,3 +172,17 @@ EMAIL_FROM = 'DailyFresh<mydailyfresh@163.com>'
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 
 
+# Django的缓存配置
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://localhost:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+# 配置session存储
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
